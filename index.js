37d17f5fd8b54e8731d6c7a6a97bf224c18d8123
leelizs <leolorenz159@gmail.com>
2023-05-29 23:51:49 -0300

    javascript básico

---
+++ index.js
@@ -0,0 +1,158 @@
+/* const sinal = "kkskskksks";
+
+if (sinal == "verde") {
+    console.log("pode passar!");
+}
+else if (sinal == "amarelo") {
+    console.log("cuidado!");
+}
+else if (sinal == "vermelho") {
+    console.log("o sinal está fechado!");
+}
+else {
+    console.log("por favor, digite algo válido.");
+} */
+/* const sinal = "roxo";
+
+switch (sinal) {
+    case "verde":
+        console.log("pode passar!");
+        break;
+    case "amarelo":
+        console.log("cuidado!");
+        break;
+    case "vermelho":
+        console.log("o sinal está fechado!");
+        break;
+    default: 
+        console.log("cade meu pai?????????????????????????????????????????????")
+} */
+/* let i = 0;
+while (i < 10) {
+    console.log(i);
+    i = i + 1
+} */
+
+/* const a = ['leo','foi','abandonado','pelo','pai'];
+const b = 7;
+console.log(a[3]);
+console.log(a);
+a.push('meu pai sumiu');
+console.log(a);
+console.log(a.length);
+
+for (let i = 0; i < a.length; i = i + 1) {
+    console.log(a[i]);
+} */
+
+ /* let a = {
+    nome: 'leo',
+    sobrenome: 'melo',
+    endereco: {
+        rua: 'parapuã',
+        numero: '330'
+    },
+    nascimento: {
+        data: '20/06/2005'
+    }
+}
+/* a = ['leo','foi','abandonado','pelo','pai']; */
+/* console.log(a.nascimento); */
+
+/* const notas = [1,2,3,9];
+
+let soma = 0;
+
+for (let i = 0; i < notas.length; i = i + 1) {
+    soma = soma + notas[i]
+}
+const media = soma / notas.length;
+
+console.log(media); */
+
+/* const a = [];
+
+a.push(2);
+
+a.push(3);
+
+a.push(4);
+
+a.push(5);
+
+let resultado;
+resultado = 1;
+// [2,3,4,5]
+for (let i = 0; i <= a.length - 1; i = i + 1) {
+    console.log(resultado);
+    resultado = resultado * a[i];
+    console.log(resultado);
+}  */
+
+// i = 0; resultado = 1; resultado = 2;
+// i = 1; resultado = 2; resultado = 6;
+// i = 2; resultado = 6; resultado = 24;
+// i = 3; resultado = 24; resultado = 120;
+
+/* for (let i = 20; i < 100; i = i + 1) {
+    console.log("olá mundo");
+    console.log(i);
+} */
+
+// iteração 0; i = 0;
+// iteração 1; i = 1;
+// primeiro executa a inicialização
+// segundo executa a condição
+// terceiro executa os comandos do for
+// quarto executa o ecremento
+// quinto executa a condição
+// sexto executa os comandos do for
+// sétimo executa o ecremento
+
+/* for (let i = 35; i <= 400; i = i + 1) {
+    console.log(i + 10);
+} */
+
+/* function teste (a,b) {
+    console.log(a);
+    console.log(b);
+}
+
+teste ("pai?", "bebida?");
+teste ("por que", "cigarro?");
+teste ("me abandonou?", "mulher?"); */
+
+/* function teste (a,b,c) {
+    for (let i = a; i < b; i = i + 1) {
+        console.log(c);
+        console.log(i);
+    }
+}
+
+teste(0,10,"pai?"); */
+
+/* const p = [];
+
+p.push(2);
+p.push(3);
+p.push(4);
+p.push(5);
+
+console.log(p);
+
+let resultado;
+resultado = 1;
+
+for (let i = 1; i <= p.length - 1; i = i + 1 ) {
+    console.log(resultado);
+    resultado = resultado * p[i];
+    console.log(resultado);
+} */
+
+const nome = "banguela";
+
+if(nome == "banguela") {
+    console.log("oi, banguela!");
+} else {
+    console.log("saia falsa");
+}
---
+++ teste.js
@@ -0,0 +1,54 @@
+/* console.log("eai");
+let a = 2
+a = 3
+console.log(a);
+const b = true;
+const c = false;
+const d = 2
+const e = 'eae';
+const f = a ** (1/2)
+const g = a + e
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g); */
+
+/* const a = 'anderson';
+const b = 'são paulo';
+const c = a + ' ' + 'mora em:' + ' ' + b
+const d = `${a} mora em: ${b}`
+const e = 10;
+
+console.log(d);
+
+if (e < 10) {
+console.log("parabéns!")
+} else {
+    console.log("vc viu meu pai?, vc viu meu pai?, vc viu meu pai?")
+} */
+
+// && ele é verdadeiro sempre que os dois operandos são verdadeiros. 
+// e ele é falso sempre que pelo menos um dos operandos por falso.
+// || ele é verdadeiro se pelo menos um dos operandos for verdadeiro.
+
+/* if (false) {
+    console.log("eai");
+}
+
+const a = 12 >= 11 || (11 == 11 && 13 == 13);
+console.log(a); */
+// true || true && true
+// true && true = true
+
+// true || (true && true)
+// true || true
+// true
+
+/* for (let i = 996; i <= 1000; i = i + 1) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+    
+} */
