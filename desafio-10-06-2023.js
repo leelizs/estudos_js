95c96c154c12c7ecf3d8a892b80a70cf54cdc442
leelizs <leolorenz159@gmail.com>
2023-06-10 20:43:23 -0300

    exercicios javascript

---
+++ desafio-10-06-2023.js
@@ -0,0 +1,90 @@
+/* const alunos = [];
+
+alunos.push({
+    nome: "Léo",
+    nota1: 6,
+    nota2: 8,
+    nota3: 9,
+    nota4: 4,
+    media: 0
+})
+
+alunos.push({
+    nome: "Anderson",
+    nota1: 7,
+    nota2: 7,
+    nota3: 8,
+    nota4: 9,
+    media: 0
+})
+
+console.log(alunos);
+
+function media(a, b, c, d) {
+    return (a + b + c + d) / 4;
+}
+
+for (let i = 0; i <= alunos.length - 1; i = i + 1) {
+    alunos[i].media = media(alunos[i].nota1, alunos[i].nota2,
+        alunos[i].nota3, alunos[i].nota4);
+}
+
+console.log(alunos); */
+
+/* const num = [4,2,5,6];
+
+ function soma(a) {
+    let soma = 1;
+    for(let i = 0; i <= a.length - 1; i = i + 1 ) {
+        soma = soma * a[i];
+    }
+    return soma;
+}
+
+const resultado = soma(num);
+
+console.log(resultado); */
+
+// exercício 1
+
+/* function hello(a) {
+    return a;
+}
+
+const print = hello("bom dia, mundo!");
+
+console.log(print); */
+
+// exercício 2
+
+/* function calcular(num) {
+    return num*num*num;
+}
+
+// exercício 3
+
+const resultado = calcular(6);
+
+console.log(resultado); */
+
+/* function converter(f) {
+    let celsius = (f - 32) * (5 / 9)
+    return celsius;
+}
+
+const resultado = converter(212);
+
+console.log(resultado + "C"); */
+
+// exercício 4
+
+/* function areaT(a, b) {
+    let area = (a * b) / 2;
+    return area;
+}
+
+const resultado = areaT(5, 15);
+console.log("Área: " + resultado + "cm²"); */
+
+// exercício 5
+
