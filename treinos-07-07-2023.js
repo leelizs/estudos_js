deed941305f2fb50f31c7822457e0426941a494b
leelizs <leolorenz159@gmail.com>
2023-07-14 21:55:33 -0300

    é, tá ino

--- treinos-07-07-2023.js
+++ treinos-07-07-2023.js
@@ -19,14 +19,38 @@ function embaralhar() {
 embaralhar();
 const palavraSorteada = palavras[0];
 const separarPalavra = palavraSorteada.split("");
+const palavraVirada = separarPalavra.map(item => ({
+    letra: item,
+    status: false
+}) );
 
-function esconderPalavra() {
+function desenharForca() {
+    let forca = "";
+    for(let i = 0; i < palavraVirada.length; i++) {
+        forca = `${forca}${palavraVirada[i].status === false ? "-" : palavraVirada[i].letra}`
+    }
+    console.log(forca);
+}
+
+
+
+function revelaLetras(letra) {
+    for(let i = 0; i < palavraVirada.length; i++) {
+        if(palavraVirada[i].letra === letra) {
+            palavraVirada[i].status = true;
+        }
+    }
+}
+
+desenharForca();
+revelaLetras("a");
+desenharForca();
+
+/* function esconderPalavra() {
     let esconder = "-";
     for(let i = 0; i < palavraSorteada.length; i++) {
         separarPalavra[i] = esconder;
     }
     return separarPalavra;
 
-}
-const resultado =  esconderPalavra();
-console.log(resultado);
+} */
